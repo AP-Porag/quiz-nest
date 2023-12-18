a581f2b201fe178c6167121ba06fadd8bce48a74
AP-Porag <smauhp@gmail.com>
2023-12-18 13:36:42 +0600

    first commit

---
+++ api/src/app.service.ts
@@ -0,0 +1,11 @@
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class AppService {
+  getHello(): string {
+    return 'Hello World!';
+  }
+  getService(): string {
+    return 'something from service';
+  }
+}
